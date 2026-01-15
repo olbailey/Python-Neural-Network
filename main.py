e76273a358bf11a58d341e031b54469127ee5c80
OliverB-963 <olbailey@outlook.com>
2026-01-15 22:18:55 +0000

    added comment

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from neural_network import NeuralNetwork
 
 BATCH_SIZE = 64
 
-# Circle Classification - loss = 0.02 at 11000 iterations
+# Circle Classification - loss = 0.02 at 11000 iterations - accuracy = 99.8%
 # data, labels = CsvReader.get_data_circle_classification("data/centralCircleLarge.csv")
 # boby = NeuralNetwork(data, labels, (3, 64, 2), 0.0003, BATCH_SIZE)
 
+# MNIST Classification - loss = 0.42 after 70 iterations - accuracy = 93.6%
 data, labels = CsvReader.get_mnist_data("data/mnist_train.csv", "data/mnist_test.csv")
-
 boby = NeuralNetwork(data, labels, (784, 256, 128, 10), 0.0003, BATCH_SIZE)
 
 boby.train()
