ac27c3b2971b7b41974195deacc3398a61f752cc
OliverB-963 <olbailey@outlook.com>
2026-01-15 22:18:55 +0000

    added comment

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ BATCH_SIZE = 64
 # data, labels = CsvReader.get_data_circle_classification("data/centralCircleLarge.csv")
 # boby = NeuralNetwork(data, labels, (3, 64, 2), 0.0003, BATCH_SIZE)
 
+# MNIST Classification - loss = 0.42 after 70 iterations
 data, labels = CsvReader.get_mnist_data("data/mnist_train.csv", "data/mnist_test.csv")
 
 boby = NeuralNetwork(data, labels, (784, 256, 128, 10), 0.0003, BATCH_SIZE)
