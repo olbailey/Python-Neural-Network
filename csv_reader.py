73be737d4064765116d641bbb6319f3adb0e3e2a
OliverB-963 <olbailey@outlook.com>
2026-01-15 18:25:54 +0000

    added extra input node, Got loss of 0.024 and accuracy of 99.4% on large dataset

--- csv_reader.py
+++ csv_reader.py
@@ -4,9 +4,16 @@ import pandas as pd
 class CsvReader:
     def get_data(file_name: str) -> tuple[np.ndarray, np.ndarray]:
         df = pd.read_csv(file_name)
-        data = np.array(df[["x", "y"]])
         labels = np.array(df["label"])
+        data = np.array(df[["x", "y"]])
+
+        data_squared = data ** 2
+        extra_info = data_squared[:, 0] + data_squared[:, 1] # x^2 + y^2
+        extra_info = np.sqrt(extra_info)
+
+        data = np.column_stack((data, extra_info))
         return data, labels
 
 if __name__ == "__main__":
-    CsvReader.get_data("data/centralCircleSmall.csv")
+    x, y = CsvReader.get_data("data/centralCircleSmall.csv")
+    print(x[54])
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from neural_network import NeuralNetwork
 
 BATCH_SIZE = 64
 
-data, labels = CsvReader.get_data("data/centralCircleSmall.csv")
+data, labels = CsvReader.get_data("data/centralCircleSmaller.csv")
 
-boby = NeuralNetwork(data, labels, (2, 16, 2), 0.0003, BATCH_SIZE)
+boby = NeuralNetwork(data, labels, (3, 64, 2), 0.0003, BATCH_SIZE)
 boby.train()
